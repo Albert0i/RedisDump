4b5f366b0dcea09c65938aff8e02558e4a14845a
Alberto Iong <albert0i@hotmail.com>
2025-07-23 17:54:35 +0800

    write more

--- src/myLib.lua.bak.lua
+++ src/myLib.lua.bak.lua
@@ -1,36 +1,33 @@
 #!lua name=mylib
 
-redis.register_function('ver', function(KEYS, ARGV) 
+redis.register_function(ver, function(KEYS, ARGV) 
     return redis.REDIS_VERSION 
 end )
 
 redis.register_function('countKeys', function(KEYS, ARGV)
-    -- KEYS[1] = prefix pattern (e.g., "user:*")
-    local cursor = "0"
-    local totalCount = 0  
-    local totalSize = 0   
-
+    -- KEYS[1] = prefix pattern (e.g., user:*)
+    local cursor = 0
+    local totalCount = 0
+    local totalSize = 0
 
     repeat
-    local result = redis.call("SCAN", cursor, "MATCH", KEYS[1], "COUNT", 1000)
+    local result = redis.call(SCAN, cursor, MATCH, KEYS[1], COUNT, 1000)
     cursor = result[1]
     local keys = result[2]
 
     for i = 1, #keys do
         totalCount = totalCount + 1
-
-        -- The unit returned by Redis’s MEMORY USAGE command is bytes
-        local size = redis.call("MEMORY", "USAGE", keys[i])
+        local size = redis.call(MEMORY, USAGE, keys[i])
         if size then
         totalSize = totalSize + size
         end
     end
-    until cursor == "0"
+    until cursor == 0
 
     return { totalCount, totalSize }
 end )
 
-redis.register_function('zAddIncr', function(KEYS, ARGV) 
+redis.register_function(zAddIncr, function(KEYS, ARGV) 
     -- KEYS[1] = sorted set key
     -- ARGV[1] = member name
 
@@ -45,7 +42,7 @@ redis.register_function('zAddIncr', function(KEYS, ARGV)
     end
 end )
 
-redis.register_function('zSumScore', function(KEYS, ARGV) 
+redis.register_function(zSumScore, function(KEYS, ARGV) 
     -- KEYS[1] = sorted set key
 
     local total = 0
@@ -58,18 +55,18 @@ redis.register_function('zSumScore', function(KEYS, ARGV)
     return total
 end )
 
-redis.register_function('scanTextChi', function(KEYS, ARGV) 
+redis.register_function(scanTextChi, function(KEYS, ARGV) 
     --[[
-    Lua script to scan Redis for hashes matching "document:*"
+    Lua script to scan Redis for hashes matching document:*
     and return HASH objects.
 
     Parameters:
-        KEYS[1] - Key pattern to scan for, "documents:" for example;
-        KEYS[2] - Field name to scan for, "textChi" for example;
-        KEYS[3] - Value to scan for, "韓非子" for example; 
+        KEYS[1] - Key pattern to scan for, documents: for example;
+        KEYS[2] - Field name to scan for, textChi for example;
+        KEYS[3] - Value to scan for, 韓非子 for example; 
         KEYS[4] - The number of documents to skip, '0' for example; 
         KEYS[5] - The maximum number of documents to return, '10' for example; 
-        ARGV[] - Fields to be returned, ["id", "textChi", "visited"] for example.
+        ARGV[] - Fields to be returned, [id, textChi, visited] for example.
 
     Returns:
         Array of array contains the documents.
@@ -77,19 +74,19 @@ redis.register_function('scanTextChi', function(KEYS, ARGV)
     local offset = tonumber(KEYS[4])
     local limit = tonumber(KEYS[5])
 
-    local cursor = "0"  -- the cursor.
+    local cursor = 0  -- the cursor.
     local matched = {}  -- result to be returned 
     local index = 1     -- index to place retrieved value
 
     repeat
-    local scan = redis.call("SCAN", cursor, "MATCH", KEYS[1], "COUNT", 100)
-    -- "scan" returns [cursor, keys] 
+    local scan = redis.call(SCAN, cursor, MATCH, KEYS[1], COUNT, 100)
+    -- scan returns [cursor, keys] 
     cursor = scan[1]
     local keys = scan[2]
 
     for _, key in ipairs(keys) do
         -- Get the field value to inspect 
-        local text = redis.call("HGET", key, KEYS[2])
+        local text = redis.call(HGET, key, KEYS[2])
         
         -- If found and contains the value
         if (text) and (string.find(text, KEYS[3])) then 
@@ -100,10 +97,10 @@ redis.register_function('scanTextChi', function(KEYS, ARGV)
             -- Take limit 
             if limit > 0 then 
             -- If no field names specified to return 
-            if ARGV[1] == "*" then
-                matched[index] = redis.call("HGETALL", key)
+            if ARGV[1] == * then
+                matched[index] = redis.call(HGETALL, key)
             else        
-                matched[index] = redis.call("HMGET", key, unpack(ARGV))
+                matched[index] = redis.call(HMGET, key, unpack(ARGV))
             end
 
             -- Increase the index 
@@ -117,23 +114,23 @@ redis.register_function('scanTextChi', function(KEYS, ARGV)
         end 
         end 
     end
-    until (cursor == "0") -- Loop until no more keys found
+    until (cursor == 0) -- Loop until no more keys found
 
     -- Scan completed
     return matched
 end )
 
-redis.register_function('fsTextChi', function(KEYS, ARGV) 
+redis.register_function(fsTextChi, function(KEYS, ARGV) 
     --[[
-    Lua script uses ZINTERSTORE to calculate hashes matching "document:*"
+    Lua script uses ZINTERSTORE to calculate hashes matching document:*
     and return HASH objects.
 
     Parameters:
-        KEYS[1] - Field name contains the text, "textChi" for example;
-        KEYS[2] - Value contained, "韓非子" for example; 
+        KEYS[1] - Field name contains the text, textChi for example;
+        KEYS[2] - Value contained, 韓非子 for example; 
         KEYS[3] - The number of documents to skip, '0' for example; 
         KEYS[4] - The maximum number of documents to return, '10' for example; 
-        ARGV[]  - list of source keys, ["fts:chinese:tokens:世", "fts:chinese:tokens:界"] for example.
+        ARGV[]  - list of source keys, [fts:chinese:tokens:世, fts:chinese:tokens:界] for example.
 
     Returns:
         Array of array contains the documents.
@@ -179,15 +176,15 @@ redis.register_function('fsTextChi', function(KEYS, ARGV)
 
     -- If intersect is not empty 
     if ( n > 0 ) then 
-    -- ZREVRANGEBYSCORE "temp:世界" +inf -inf WITHSCORES
+    -- ZREVRANGEBYSCORE temp:世界 +inf -inf WITHSCORES
     local z = redis.call('ZREVRANGEBYSCORE', tempkey, '+inf', '-inf', 'WITHSCORES')
-    -- Example result: { "userA", "42", "userB", "37", "userC", "29" }
+    -- Example result: { userA, 42, userB, 37, userC, 29 }
     for i = 1, #z, 2 do
         local key = z[i]
         local score = tonumber(z[i + 1])
 
         -- Get the field value to inspect 
-        local text = redis.call("HGET", key, KEYS[1])
+        local text = redis.call(HGET, key, KEYS[1])
 
         -- If found and contains the value
         if (text) and (string.find(text, KEYS[2])) then 
@@ -197,7 +194,7 @@ redis.register_function('fsTextChi', function(KEYS, ARGV)
         else 
             -- Take limit 
             if limit > 0 then 
-            matched[index] = { redis.call("HGETALL", key), score }
+            matched[index] = { redis.call(HGETALL, key), score }
 
             -- Increase the index 
             index = index + 1
@@ -216,15 +213,15 @@ redis.register_function('fsTextChi', function(KEYS, ARGV)
     return matched
 end )
 
-redis.register_function('getVisitedDocs', function(KEYS, ARGV) 
+redis.register_function(getVisitedDocs, function(KEYS, ARGV) 
     --[[
     Lua script to fetch Hash Entries from a Sorted Set by Score
 
     Parameters:
-        KEYS[1] - Name of the Sorted set, "fts:documents:visited for example
+        KEYS[1] - Name of the Sorted set, fts:documents:visited for example
         KEYS[2] - The number of documents to skip, '0' for example; 
         KEYS[3] - The maximum number of documents to return, '10' for example; 
-        ARGV[] - Fields to be returned, ["id", "textChi", "visited"] for example.
+        ARGV[] - Fields to be returned, [id, textChi, visited] for example.
 
     Returns:
         Array of array contains the documents.
@@ -236,16 +233,16 @@ redis.register_function('getVisitedDocs', function(KEYS, ARGV)
     local index = 1     -- index to place retrieved value
 
     -- Read members from high to low score
-    local keys = redis.call("ZREVRANGEBYSCORE", KEYS[1], '+inf', '-inf', 'LIMIT', offset, limit)
+    local keys = redis.call(ZREVRANGEBYSCORE, KEYS[1], '+inf', '-inf', 'LIMIT', offset, limit)
 
     for _, key in ipairs(keys) do
         -- Take limit 
         if limit > 0 then 
         -- If no field names specified to return 
-        if ARGV[1] == "*" then
-            matched[index] = redis.call("HGETALL", key)
+        if ARGV[1] == * then
+            matched[index] = redis.call(HGETALL, key)
         else        
-            matched[index] = redis.call("HMGET", key, unpack(ARGV))
+            matched[index] = redis.call(HMGET, key, unpack(ARGV))
         end
 
         -- Increase the index 
