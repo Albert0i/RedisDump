58553b0f52dd0fbbd9ec38f367519c22dfc28bea
Alberto Iong <albert0i@hotmail.com>
2025-07-30 12:56:18 +0800

    write more

--- src/myLib.lua
+++ src/myLib.lua
@@ -8,7 +8,7 @@ local function ver(KEYS, ARGV)
   return redis.REDIS_VERSION
 end
 
--- Lua libraries 
+-- Lua Runtime libraries 
 -- No parameter is required:
 -- Example usage: FCALL_RO LIBS 0
 -- Output:  1) " 1) _G              [Variable]"
@@ -16,7 +16,7 @@ end
 --          3) " 3) assert          [Function]"
 --          4) " 4) bit             [Variable]"
 --          5) " 5) cjson           [Variable]"
---          6) " 6) cmsgpack        [Variable]"      
+--          6) " 6) cmsgpack        [Variable]"
 local function libs(KEYS, ARGV)
   local libs = {}
   for k, v in pairs(_G) do
@@ -38,6 +38,12 @@ local function libs(KEYS, ARGV)
   return result
 end 
 
+-- Return RESP3 object
+-- No parameter is required:
+-- Example usage: FCALL_RO RESP3 0
+-- Output:  "{ name = "iong_dev", status = "active", score = 98 }" if RESP3 is supported; 
+--          "[]" if RESP3 is not supported.
+
 local function resp3(KEYS, ARGV)
   redis.setresp(3)
   return { name = "iong_dev", status = "active", score = 98 }  
@@ -57,6 +63,7 @@ local function toFix(KEYS, ARGV)
 
   return string.format("%." .. digits .. "f", n)
 end
+
 
 -- Count number of keys and size of a pattern
 -- Optional:
@@ -115,6 +122,7 @@ local function delall(KEYS, ARGV)
   return deletedCount
 end
 
+
 -- Add member to Sorted Set
 -- Required:
 --      KEYS[1] = Sorted Set key
@@ -166,46 +174,85 @@ local function zSumScore(KEYS, ARGV)
   return total
 end
 
---[[
-  Lua script to scan Redis for hashes matching "document:*"
-  and return HASH objects.
+--
+-- Experimental !!!
+-- 
+local function debugPrint(message)
+  redis.log(redis.LOG_WARNING, 'DEBUG > ' .. (message or ''))
+end
 
-  Required:
-      KEYS[1] - Key pattern to scan for, "documents:" for example;
-      KEYS[2] - Field name to scan for, "textChi" for example;
-      KEYS[3] - Value to scan for, "韓非子" for example; 
-  Optional:
-      KEYS[4] - The number of documents to skip, 0 if unspecified; 
-      KEYS[5] - The maximum number of documents to return, 10 if unspecified; 
-      ARGV[] - Fields to be returned, ["id", "textChi", "visited"] for example. 
-              All fields will be returned if unspecified.
-  Returns:
-      Array of array contains the documents.
-  Example usage: 
-      FCALL_RO SCANTEXTCHI 5 fts:chinese:documents:* key 鄭文公 0 10  id textChi visited
-      FCALL_RO SCANTEXTCHI 3 fts:chinese:documents:* key 鄭文公
-  Output: 
---]]
+local function table_to_string(tbl)
+  local parts = {}
+  for k, v in pairs(tbl) do
+    local key = tostring(k)
+    local val = tostring(v)
+
+    if (val == "") then 
+      val = "nil"
+    end
+    table.insert(parts, key .. " = '" .. val .. "'")
+  end
+  return "{" .. table.concat(parts, ", ") .. "}"
+end
+
+-- Builds a key-value table from two equally sized input tables
+local function array_to_map(field_name_table, field_value_table)
+  local result = {}
+
+  debugPrint()
+  debugPrint("field_name_table: " .. table_to_string(field_name_table))
+  debugPrint("field_value_table: " .. table_to_string(field_value_table))
+
+  for i = 1, #field_name_table do
+    result[field_name_table[i]] = field_value_table[i]
+  end
+
+  debugPrint("result_table: " .. table_to_string(result))
+  return result
+end
+
+-- 
+-- Return Redis hashes matching a pattern and has a field which contains a value,
+-- 
+--  Required:
+--      KEYS[1] - Key pattern to scan for, "documents:*" for example;
+--      KEYS[2] - Field name to check for, "textChi" for example;
+--      KEYS[3] - Value to to check for, "韓非子" for example; 
+--  Optional:
+--      KEYS[4] - Number of documents to skip, 0 if unspecified; 
+--      KEYS[5] - Maximum number of documents to return, 10 if unspecified; 
+--      ARGV[]  - Fields to return, ["id", "textChi", "visited"] for example,
+--                Return all fields if unspecified.
+--  Returns:
+--      Array of array contains the documents.
+--  Example usage: 
+--      FCALL_RO SCANTEXTCHI 5 fts:chinese:documents:* key 鄭文公 0 10  id textChi visited
+--      FCALL_RO SCANTEXTCHI 3 fts:chinese:documents:* key 鄭文公
+--  Output: 
+--
 local function scanTextChi(KEYS, ARGV)
+  local keyPrefix = KEYS[1]
+  local fieldName = KEYS[2]
+  local checkValue = KEYS[3]
   local offset = tonumber(KEYS[4]) or 0
   local limit = tonumber(KEYS[5]) or 10
-
+  
   local cursor = "0"  -- the cursor.
   local matched = {}  -- result to be returned 
   local index = 1     -- index to place retrieved value
 
   repeat
-  local scan = redis.call("SCAN", cursor, "MATCH", KEYS[1], "COUNT", 100)
+  local scan = redis.call("SCAN", cursor, "MATCH", keyPrefix, "COUNT", 100)
   -- "scan" returns [cursor, keys] 
   cursor = scan[1]
   local keys = scan[2]
 
   for _, key in ipairs(keys) do
       -- Get the field value to inspect 
-      local text = redis.call("HGET", key, KEYS[2])
+      local text = redis.call("HGET", key, fieldName)
       
       -- If found and contains the value
-      if (text) and (string.find(text, KEYS[3])) then 
+      if (text) and (string.find(text, checkValue)) then 
       -- Skip offset 
       if offset > 0 then 
           offset = offset - 1
