9d5e715b06c525d21ca8ec177d7ab674375fc411
Alberto Iong <albert0i@hotmail.com>
2025-08-01 10:14:51 +0800

    write more

--- src/resp3test.js
+++ src/resp3test.js
@@ -4,25 +4,19 @@ import { redis } from './redis/redis.js'
    main 
 */
 const script = `
-    local function table_to_array(tbl)
-      local arr = {}
-      for k, v in pairs(tbl) do
-        table.insert(arr, tostring(k))
-        table.insert(arr, v)
-      end
-      return arr
-    end
-
     redis.setresp(3)
-    return { name = "iong_dev", status = "active", score = 98 }
+    return cjson.encode({ name = "iong_dev", status = "active", score = 98 })
   `
 await redis.connect()
 
 await redis.sendCommand(['HELLO', '3']);
 console.log(await redis.sendCommand(['HELLO']));
 
-console.log('The result is', 
-    await redis.eval(script, { keys: [], arguments: [] }))
+const result = await redis.eval(script, { keys: [], arguments: [] })
+const data = JSON.parse(result)
+
+console.log('The result is', result)
+console.log('The data is', data)
 
 await redis.close();
 process.exit(0)
